d3c3b956efe857983cc0c38f78fef25c6b648eba
EfforiaKnight <tualet00@gmail.com>
2017-12-15 18:31:00 +0200

    fixed command line parse func

--- functions/__fzf_commandline_parse.fish
+++ functions/__fzf_commandline_parse.fish
@@ -1,7 +1,7 @@
 function __fzf_commandline_parse --description 'Parse commandline'
     set -l commandline (string split " " (commandline))
     type -qf $commandline[1]
-    if [ (count $commandline) -gt 1 -a $status -eq 0 ]
+    if [ $status -eq 0 -a (count $commandline) -gt 1 ]
         echo $commandline[2..-1]
     else
         echo $commandline
